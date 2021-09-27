850f5c33149b4754f3c1594f8846744e8f8dbd67
SimpllleUser <pecheritsa.vv@gmail.com>
2021-09-27 20:40:48 +0300

    feat: add validation on create task

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,6 +1,13 @@
+import { IsNumberString, IsString, Length } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString({ message: 'Должно быть строкой' })
+  @Length(1, 1000, { message: 'Не меньше 1' })
   title: string;
+  @IsString({ message: 'Должно быть строкой' })
+  @Length(4, 1000, { message: 'Не меньше 4 и не больше 1000' })
   description: string;
+  @IsNumberString()
   authorId: number;
   typeId: number;
   priorityId: number;
